e0d9a42e9d97234aa3a78bd4f1df4b65ac5d643b
Evrad13 <evrad237@hotmail.com>
2024-10-25 16:57:03 +0100

    application migration de authentication

--- devblog/authentication/models.py
+++ devblog/authentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    CREATOR = 'CREATOR'
+    SUBSCRIBED = 'SUBSCRIBED'
+
+    CHOICE_ROLE = (
+
+        (CREATOR, 'créateur'),
+
+        (SUBSCRIBED, 'abonné'),
+    )
+
+    profile_foto = models.ImageField(verbose_name='phote de profile')
+    role = models.CharField(max_length=30, choices=CHOICE_ROLE, verbose_name='role utilisateur')
